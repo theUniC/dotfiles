6cec9a61c9cb16d0161790cb35ece7c6903cd47f
theUniC <theunic@gmail.com>
2023-04-26 16:26:33 +0200

    Install Spacevim

--- run_once_before_first-run.sh
+++ run_once_before_first-run.sh
@@ -8,3 +8,6 @@ chsh -s /opt/homebrew/bin/zsh
 
 # Install Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install Spacevim
+curl -sLf https://spacevim.org/install.sh | bash
