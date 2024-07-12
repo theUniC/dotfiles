568a3a091bc16f6069e5e3b108acfe57177740aa
theUniC <theunic@gmail.com>
2024-07-12 14:09:19 +0200

    First run script

---
+++ first-run.sh
@@ -0,0 +1,16 @@
+#!/bin/env bash
+
+# Install all packages
+brew bundle
+
+# Change default shell to ZSH
+chsh -s /opt/homebrew/bin/zsh
+
+# Install Oh-My-Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install Spacevim
+curl -sLf https://spacevim.org/install.sh | bash
+
+# Prepare workspace directory
+mkdir -p ~/workspace
